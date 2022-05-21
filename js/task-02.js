860e3a42a217937ef0287d470855e6c47e965221
DajanaAugustowicz <dajana.augustowicz@gmail.com>
2022-05-21 21:57:19 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,7 @@
+"use strict";
+
+const list = document.querySelector("#ingredients");
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +10,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const markup = ingredients
+  .map((ingredient) => `<li class="list-item new">${ingredient}</li>`)
+  .join("");
+
+list.insertAdjacentHTML("afterbegin", markup);
+
+//ingredients.forEach(function (ingredient, index) {
+// console.log(ingredient, index)
+//});
+
+//const item = document.createElement("li");
+//item.textContent = [...ingredients];
+//item.classList.add("item");
+//list.after(item);
+
+//console.log(item);
