0d0e58ab6a8a996723a9b250e752a2ae68c71621
DajanaAugustowicz <dajana.augustowicz@gmail.com>
2022-09-26 23:05:11 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+"use strict"; 
+
+const button = document.querySelector(".change-color"); 
+const span = document.querySelector(".color"); 
+const body = document.body; 
+
+button.addEventListener("click", (event) => { 
+  const buttonValue = getRandomHexColor();
+  body.style.backgroundColor = buttonValue; 
+  span.textContent = buttonValue; 
+}); 
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+} 
