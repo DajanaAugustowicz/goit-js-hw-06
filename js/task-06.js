e89e26435a6a0d19926633fabb5fd09ef6332014
DajanaAugustowicz <dajana.augustowicz@gmail.com>
2022-09-26 20:23:24 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,3 +1,37 @@
 "use strict"; 
 
-const input = document.querySelector("#validation-input");
+const input = document.querySelector("#validation-input"); 
+
+
+
+
+
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+
+input.addEventListener("input", (event) => { 
+    if (event.currentTarget.value < document.querySelector(`input[data-length="6"]`)) {nameOutput.textContent = "Anonymous"} 
+    else {nameOutput.textContent = event.currentTarget.value}; 
+}); 
+
+
+
+
+
+//html: 
+//<input
+//      type="text"
+//      id="validation-input"
+//      data-length="6"
+//      placeholder="Please enter 6 symbols"
+//    /> 
+//<style>
+//#validation-input {
+//    border: 2px solid #bdbdbd;}
+
+//  #validation-input.valid {
+//    border-color: #4caf50;}
+
+//  #validation-input.invalid {
+//    border-color: #f44336;}
+//</style>
