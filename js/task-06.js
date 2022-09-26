5033fd83ba6cda6a0bd27c44ccc134799c788bf4
DajanaAugustowicz <dajana.augustowicz@gmail.com>
2022-09-26 23:29:38 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,37 +1,9 @@
 "use strict"; 
 
-const input = document.querySelector("#validation-input"); 
+const input = document.querySelector("#validation-input");  
 
-
-
-
-
-const nameInput = document.querySelector("#name-input");
-const nameOutput = document.querySelector("#name-output");
-
-input.addEventListener("input", (event) => { 
-    if (event.currentTarget.value < document.querySelector(`input[data-length="6"]`)) {nameOutput.textContent = "Anonymous"} 
-    else {nameOutput.textContent = event.currentTarget.value}; 
-}); 
-
-
-
-
-
-//html: 
-//<input
-//      type="text"
-//      id="validation-input"
-//      data-length="6"
-//      placeholder="Please enter 6 symbols"
-//    /> 
-//<style>
-//#validation-input {
-//    border: 2px solid #bdbdbd;}
-
-//  #validation-input.valid {
-//    border-color: #4caf50;}
-
-//  #validation-input.invalid {
-//    border-color: #f44336;}
-//</style>
+input.addEventListener("blur", () => {
+    if (input.value.length < 6) { 
+        input.style.borderColor = "#f44336"; 
+    } else {input.style.borderColor = "#4caf50";}
+  });
