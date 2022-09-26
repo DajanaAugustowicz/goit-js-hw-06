4a92be297afa7cb8f82550099328a977fd48c42c
DajanaAugustowicz <dajana.augustowicz@gmail.com>
2022-09-26 20:23:27 +0200

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const textInput = document.querySelector("#font-size-control");
+const output = document.querySelector("#text");
+
+textInput.addEventListener("input", (event) => {
+  output.textContent = event.currentTarget.value;
+}); 
+
+output.style.fontSize i po = `${ event.target.value}px`;
